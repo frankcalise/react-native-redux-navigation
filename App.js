185a702e66c01c332e9c13876be07e6a834f336c
Frank Calise <fcalise@gmail.com>
2020-01-16 10:44:42 -0500

    Switch navigation

--- App.js
+++ App.js
@@ -1,94 +1,100 @@
-import * as React from "react";
-import { Button, Text, View, StyleSheet } from "react-native";
-import { Provider, connect } from "react-redux";
-import { createAppContainer } from "react-navigation";
+import React from "react";
+import {
+  ActivityIndicator,
+  AsyncStorage,
+  Button,
+  StatusBar,
+  StyleSheet,
+  View
+} from "react-native";
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import { createStore, combineReducers } from "redux";
 
-// A very simple reducer
-function counter(state, action) {
-  if (typeof state === "undefined") {
-    return 0;
-  }
-
-  switch (action.type) {
-    case "INCREMENT":
-      return state + 1;
-    case "DECREMENT":
-      return state - 1;
-    default:
-      return state;
-  }
-}
-
-// A very simple store
-let store = createStore(combineReducers({ count: counter }));
-
-// A screen!
-class Counter extends React.Component {
+class SignInScreen extends React.Component {
   static navigationOptions = {
-    title: "Counter!"
+    title: "Please sign in"
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>{this.props.count}</Text>
-        <Button
-          title="Increment"
-          onPress={() => this.props.dispatch({ type: "INCREMENT" })}
-        />
-        <Button
-          title="Decrement"
-          onPress={() => this.props.dispatch({ type: "DECREMENT" })}
-        />
-
-        <Button
-          title="Go to static count screen"
-          onPress={() => this.props.navigation.navigate("StaticCounter")}
-        />
+        <Button title="Sign in!" onPress={this._signInAsync} />
       </View>
     );
   }
+
+  _signInAsync = async () => {
+    await AsyncStorage.setItem("userToken", "abc");
+    this.props.navigation.navigate("App");
+  };
 }
 
-// Another screen!
-class StaticCounter extends React.Component {
+class HomeScreen extends React.Component {
   static navigationOptions = {
-    title: `Same number, wow!`
+    title: "Welcome to the app!"
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.paragraph}>{this.props.count}</Text>
+        <Button title="Show me more of the app" onPress={this._showMoreApp} />
+        <Button title="Actually, sign me out :)" onPress={this._signOutAsync} />
       </View>
     );
   }
+
+  _showMoreApp = () => {
+    this.props.navigation.navigate("Other");
+  };
+
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate("Auth");
+  };
 }
 
-// Connect the screens to Redux
-let CounterContainer = connect(state => ({ count: state.count }))(Counter);
-let StaticCounterContainer = connect(state => ({ count: state.count }))(
-  StaticCounter
-);
+class OtherScreen extends React.Component {
+  static navigationOptions = {
+    title: "Lots of features here"
+  };
 
-// Create our stack navigator
-let RootStack = createStackNavigator({
-  Counter: CounterContainer,
-  StaticCounter: StaticCounterContainer
-});
-
-// And the app container
-let Navigation = createAppContainer(RootStack);
-
-// Render the app container component with the provider around it
-export default class App extends React.Component {
   render() {
     return (
-      <Provider store={store}>
-        <Navigation />
-      </Provider>
+      <View style={styles.container}>
+        <Button title="I'm done, sign me out" onPress={this._signOutAsync} />
+        <StatusBar barStyle="default" />
+      </View>
+    );
+  }
+
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate("Auth");
+  };
+}
+
+class AuthLoadingScreen extends React.Component {
+  constructor() {
+    super();
+    this._bootstrapAsync();
+  }
+
+  // Fetch the token from storage then navigate to our appropriate place
+  _bootstrapAsync = async () => {
+    const userToken = await AsyncStorage.getItem("userToken");
+
+    // This will switch to the App screen or Auth screen and this loading
+    // screen will be unmounted and thrown away.
+    this.props.navigation.navigate(userToken ? "App" : "Auth");
+  };
+
+  // Render any loading content that you like here
+  render() {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+        <StatusBar barStyle="default" />
+      </View>
     );
   }
 }
@@ -96,15 +102,23 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#ecf0f1",
-    padding: 8
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: "bold",
-    textAlign: "center"
+    justifyContent: "center"
   }
 });
+
+const AppStack = createStackNavigator({ Home: HomeScreen, Other: OtherScreen });
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
+
+export default createAppContainer(
+  createSwitchNavigator(
+    {
+      AuthLoading: AuthLoadingScreen,
+      App: AppStack,
+      Auth: AuthStack
+    },
+    {
+      initialRouteName: "AuthLoading"
+    }
+  )
+);
